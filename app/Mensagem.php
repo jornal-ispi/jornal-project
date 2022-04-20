f10087afd66a908bf2ed6505a57aad1fe7b4ba4e
Nicolau NP <mr1Normaliii@gmail.com>
2022-04-20 18:59:48 +0100

    trabalhando no modelo mensagems

--- app/Mensagem.php
+++ app/Mensagem.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mensagem extends Model
 {
-    //
+    protected $table = "mensagems";
+
+    protected $fillable = [
+        'id_user_send',
+        'id_user_receive',
+        'sms',
+        'status_sms',
+        'estado',
+    ];
+
+    public function user_send(){
+        return $this->belongsTo(User::class, 'id_user_send', 'id');
+    }
+
+    public function user_receive(){
+        return $this->belongsTo(User::class, 'id_user_receive', 'id');
+    }
 }
