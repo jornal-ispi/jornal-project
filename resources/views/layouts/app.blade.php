deec5896fb2e3d1650f41fb9e1edc0505882d109
Nicolau NP <mr1Normaliii@gmail.com>
2022-04-20 18:33:03 +0100

    trabalhando nos menus

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
                         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                             <div class="navbar-nav">
                                 <a class="nav-link @if ($menu=='Home' ) active @endif" aria-current="page" href="/">Home</a>
+                                <a class="nav-link @if ($menu=='Usuários' ) active @endif" aria-current="page" href="/usuario">Usuários</a>
                                 <a class="nav-link @if ($menu=='Notícia' ) active @endif" aria-current="page" href="/noticia">Notícia</a>
                                 <a class="nav-link @if ($menu=='Chat' ) active @endif" aria-current="page" href="/chat">Chat</a>
                                 <a class="nav-link @if ($menu=='Portefólio' ) active @endif" aria-current="page"
